a51ae9dcbb111c2068e509cd8304008dc92a562e
dark-samus <mathmcclintic@gmail.com>
2016-05-08 18:47:36 -0400

    extend the amount stage 1 copies from NAND to memory

--- payload_stage1/source/main.c
+++ payload_stage1/source/main.c
@@ -10,12 +10,12 @@ void main()
     if(i2cReadRegister(I2C_DEV_MCU, 0x10) & 0x4) //if home button is pressed
     {
         //Jump to alternate stage2
-        sdmmc_nand_readsectors(0x5A000, 0x20, (u8*)0x08006000);
+        sdmmc_nand_readsectors(0x5A000, 0x64, (u8*)0x08006000);
     }
     else
     {
         //jump to normal stage2
-        sdmmc_nand_readsectors(0x5C000, 0x20, (u8*)0x08006000);
+        sdmmc_nand_readsectors(0x5C000, 0x64, (u8*)0x08006000);
     }
     
     // Jump to secondary payload
